33df4bd5f533d6f79339fd9b7b13c88d426272ff
Jackson Booth <jacksontbooth@icloud.com>
2022-01-19 16:10:49 +1100

    added glitch effect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+// Glitch Effect From Matt Gross  
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="hero-container">
+        <div class="environment"></div>
+        <h2 class="hero glitch layers" data-text="團隊-還"><span>Team Hai</span></h2>
+      </div>
     </div>
   );
 }
